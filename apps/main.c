87af3a5bc211bdf87421a66ec8a009cf42e339c6
Matheus Aguiar <matheusaguiar293@usp.br>
2024-09-24 11:01:06 -0300

    comments

--- apps/main.c
+++ apps/main.c
@@ -1,52 +1,34 @@
+/// NOME: MATHEUS AGUIAR   -    N°USP: 14781512        -         O integrante da dupla desenvolveu 100% do que foi combinado de sua parte no trabalho 
+/// NOME: MURILO CURY PONTES  -   N°USP: 13830417        -        O integrante da dupla desenvolveu 100% do que foi combinado de sua parte no trabalho
+/// NÚMERO DO GRUPO: 11
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "my-lib.h"
+#include "sql.h"
 #include "funcoesFornecidas.h"
 
+
 int main(){
-	int command;
-	char *filename = (char *) malloc(50 * sizeof(char));
+	int command;                    // input command
+	char *filename = (char *) malloc(50 * sizeof(char));    // file name coming from input
 	scanf("%d %s",&command,filename);
 
 	switch(command){
 		case 1: {
-<<<<<<< HEAD
-			break;
-		}
-		case 2:{
-			print_info(filename);
-=======
             char *filename2 = (char *) malloc(50 * sizeof(char));
             scanf(" %s",filename2);
             read_csv(&filename,&filename2);
 			break;
 		}
 		case 2:{
-			//print_info(filename);
->>>>>>> bf634ed (pc pinkas)
+			print_info(filename);
 			break;
 		}  
 		case 3:{
-			int n;
-<<<<<<< HEAD
-			char *field = (char *) malloc(20 * sizeof(char));
-			char *value = (char *) malloc(30 * sizeof(char)); 
-			scanf(" %d",&n);
-			for(int i = 0; i < n; i++){
-				printf("Busca %d\n",i+1);
-				scanf("%s ",field);  
-				scan_quote_string(value);
-				//search_records(filename,field,value);
-				//printf("Numero de paginas de disco: %d");
-			}
-			free(field);
-			field = NULL;
-			free(value);
-			value = NULL;
-=======
-            char *field = (char *) malloc(20 * sizeof(char));
-            char *value = (char *) malloc(30 * sizeof(char));
+			int n;       // the 'n' searches
+            char *field = (char *) malloc(20 * sizeof(char));    // field coming from the input
+            char *value = (char *) malloc(30 * sizeof(char));    // field value coming from input
             scanf(" %d",&n);
             for(int i = 0; i < n; i++){
                 printf("Busca %d\n",i+1);
@@ -58,42 +40,28 @@ int main(){
             field = NULL;
             free(value);
             value = NULL;
->>>>>>> bf634ed (pc pinkas)
 			break;
 		}
 		case 4:{
 			int  n;
 			scanf(" %d", &n);
-<<<<<<< HEAD
 			search_and_delete_record(filename, n);
 			binarioNaTela(filename);
 			break;
 		}
 		case 5:{
-			break;
-		}
-		case 6:{
-			compress_file(filename);
-			binarioNaTela(filename);
-=======
-			//search_and_delete_record(filename, n);
-			//binarioNaTela(filename);
-			break;
-		}
-		case 5:{
-            int n; // n�mero de inser��es a serem realizadas
+            int n; // number of insertions to be performed
             scanf(" %d",&n);
             insert_record(&filename,n);
 			break;
 		}
 		case 6:{
-			//compress_file(filename);
-			//binarioNaTela(filename);
->>>>>>> bf634ed (pc pinkas)
+			compress_file(filename);
+			binarioNaTela(filename);
 			break;
 		}
 		default:{
-			printf("Falha no processamento do arquivo\n");
+			printf("Falha no processamento do arquivo.\n");
 			break;
 		} 
 
